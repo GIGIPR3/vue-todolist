18e414f1abe66ee451301661e8fb5dd7b8d53220
GIGIPR3 <gigiprencipe47@gmail.com>
2022-11-15 15:13:52 +0100

    .

---
+++ main.js
@@ -0,0 +1,20 @@
+var app = new Vue(
+    {
+        el: '#root',
+        data:{
+            obj: [
+                {
+                    task: 'compra il pane',
+                },
+                {
+                    task: 'compra il pane',
+                },
+            ]
+        },
+        methods:{
+            aggiungiTask(){
+
+            }
+        }
+    }
+);
